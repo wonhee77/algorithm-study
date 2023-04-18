58f1643fa3f7327051d3edac7c3431000b8f0de5
wonhee77 <leehi402@naver.com>
2023-04-18 23:50:11 +0900

    [dfs] 여행 경로

---
+++ dfs/여행경로.py
@@ -0,0 +1,28 @@
+def dfs(city, dic, route, total):
+    if len(route) == total + 1:
+        return route
+    if len(dic[city]) == 0:
+        return list()
+    cities = dic[city]
+    cities.sort()
+    for c in cities:
+        dic[city].remove(c)
+        route.append(c)
+        print(c, dic, route, total)
+        result = dfs(c, dic, route, total)
+        if len(result) == total + 1:
+            return result
+        dic[city].append(c)
+        route.pop(-1)
+def solution(tickets):
+    dic = dict()
+    total = len(tickets)
+    for ticket in tickets:
+        if ticket[0] not in dic:
+            dic[ticket[0]] = [ticket[1]]
+        else:
+            dic[ticket[0]].append(ticket[1])
+
+    return dfs("ICN", dic, ["ICN"], total)
+
+print(solution([["ICN", "BOO"], ["ICN", "COO"], ["COO", "DOO"], ["DOO", "COO"], ["BOO", "DOO"], ["DOO", "BOO"], ["BOO", "ICN"], ["COO", "BOO"]]))

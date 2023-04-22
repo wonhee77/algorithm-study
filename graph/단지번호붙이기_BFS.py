3a84a7c8384876b5621291c7364dbce25b1a09ba
wonhee77 <leehi402@naver.com>
2023-04-23 00:07:10 +0900

    [graph] 단지 번호 붙이기 BFS 추가 visited 배열을 따로 만들지 않고 숫자를 0으로 바꾸며 탐색 (백준)

--- graph/단지번호붙이기_BFS.py
+++ graph/단지번호붙이기_BFS.py
@@ -3,7 +3,6 @@ from collections import deque
 
 N = int(sys.stdin.readline())
 graph = []
-visited = [[False] * N for _ in range(N)]
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 answer = []
@@ -11,15 +10,14 @@ answer = []
 for _ in range(N):
     graph.append(list(map(int, list(sys.stdin.readline())[:-1])))
 
-
 def bfs(x, y):
     queue = deque()
     queue.append((x, y))
     cnt = 0
     while queue:
         x, y = queue.popleft()
-        if (visited[x][y] is False) & (graph[x][y] == 1):
-            visited[x][y] = True
+        if graph[x][y] == 1:
+            graph[x][y] = 0
             cnt += 1
             for i in range(4):
                 if (0 <= x + dx[i] < N) & (0 <= y + dy[i] < N):
@@ -34,8 +32,7 @@ for i in range(N):
             answer.append(cnt)
 
 print(len(answer))
-answer.sort()
-for num in answer:
+for num in sorted(answer):
     print(num)
 
 """

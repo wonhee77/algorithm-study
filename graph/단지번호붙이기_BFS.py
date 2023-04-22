639b2d8bae4c0332f70bf8cf5a3d9ac9044fd130
wonhee77 <leehi402@naver.com>
2023-04-22 23:59:14 +0900

    [graph] 단지 번호 붙이기 BFS 추가 (백준)

---
+++ graph/단지번호붙이기_BFS.py
@@ -0,0 +1,51 @@
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+graph = []
+visited = [[False] * N for _ in range(N)]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+answer = []
+
+for _ in range(N):
+    graph.append(list(map(int, list(sys.stdin.readline())[:-1])))
+
+
+def bfs(x, y):
+    queue = deque()
+    queue.append([x, y])
+    cnt = 0
+    while queue:
+        popleft = queue.popleft()
+        x, y = popleft[0], popleft[1]
+        if (visited[x][y] is False) & (graph[x][y] == 1):
+            visited[x][y] = True
+            cnt += 1
+            for i in range(4):
+                if (0 <= x + dx[i] < N) & (0 <= y + dy[i] < N):
+                    queue.append([x + dx[i], y + dy[i]])
+    return cnt
+
+
+for i in range(N):
+    for j in range(N):
+        cnt = bfs(i, j)
+        if cnt != 0:
+            answer.append(cnt)
+
+print(len(answer))
+answer.sort()
+for num in answer:
+    print(num)
+
+"""
+7
+0110100
+0110101
+1110101
+0000111
+0100000
+0111110
+0111000
+"""

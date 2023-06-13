c64a79d613e126c288271c144847d8298befa93b
wonhee77 <leehi402@naver.com>
2023-06-14 00:32:32 +0900

    feat: 가장 큰 수 (프로그래머스)

--- sorting/가장큰수.py
+++ sorting/가장큰수.py
@@ -2,3 +2,10 @@ def solution(numbers):
     numbers = list(map(str, numbers))
     numbers.sort(key=lambda x: x * 3, reverse=True)
     return str(int(''.join(numbers)))
+
+
+'''
+파이썬 정렬할 때 key를 이용하면 변형을 통한 값을 바로 정렬할 수 있다.
+! 미리 값을 조작해놓을 필요 없음
+! 파이썬에서 문자열 정렬은 왼쪽부터 아스키로 비교한다.
+'''
